ddda7aec7ed3c3929d1f39e0e480bd2e6943e60c
CanalesAlejandra2019 <alejastephanie1d@gmail.com>
2023-01-20 17:26:08 -0700

    customized homepage

---
+++ homepage/scripts/getDates.js
@@ -0,0 +1,7 @@
+ let text = document.lastModified;
+     document.getElementById("lastModified").innerHTML = text;
+
+    <script>
+    let text = document.lastModified;
+     document.getElementById("lastModified").innerHTML = text;
+ </script>
